ba323520abf9a33036aa4b82e230cd2fb9fb4705
Tulasi Ramkumar K <ramkumarkolamala2015@gmail.com>
2020-09-09 12:53:42 +0530

    added sign up with email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,7 +10,28 @@ const config = {
     storageBucket: "crwn-db-959bc.appspot.com",
     messagingSenderId: "141376726842",
     appId: "1:141376726842:web:adc43253aa8832863ad203"
-  };
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if(!snapShot.exists){
+    const {displayName, email} = userAuth;
+    const createAt = new Date();
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData 
+      })
+    }catch(error){
+      console.log('error creating user',error.message)
+    }
+  }
+  return userRef;
+};
 
   firebase.initializeApp(config);
 
